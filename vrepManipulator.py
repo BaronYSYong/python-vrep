2d954ca6f5cc4e2aac4f0fbcb02e564052fdb8f2
baron-Endeavor <baronysyong@gmail.com>
2017-05-26 17:56:10 +0900

    vrepManipulator.py: add variable argument in __init__

--- vrepManipulator.py
+++ vrepManipulator.py
@@ -2,10 +2,10 @@ import vrep
 import sys
 import math
 
-class Manipulator:
-    def __init__(self):
+class Manipulator(object):
+    def __init__(self, ip='127.0.0.1', port=19999, robot_name='UR3'):
         vrep.simxFinish(-1) # just in case, close all opened connections
-        self.ID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to V-REP
+        self.ID=vrep.simxStart(ip,port,True,True,5000,5) # Connect to V-REP
         if self.ID!=-1:
             print ('Connected to remote API server')
         else:
@@ -13,7 +13,7 @@ class Manipulator:
             sys.exit('Could not connect')
         self.jointHandles = []
         for i in range(6):
-            codeError, jointHandle = vrep.simxGetObjectHandle(self.ID, 'UR3_joint' + str(i+1), vrep.simx_opmode_oneshot_wait)
+            codeError, jointHandle = vrep.simxGetObjectHandle(self.ID, robot_name + '_joint' + str(i+1), vrep.simx_opmode_oneshot_wait)
             if codeError == 0:
                 self.jointHandles.append(jointHandle)
 
